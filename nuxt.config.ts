aa9a69130de6deb884f2ef3beccf66a43eb100f5
blurryfacebeat <blurryfacebeat@gmail.com>
2023-04-28 22:32:48 +0700

    initialize project

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'English Words Exercise',
+      meta: [{ name: 'description', content: 'Clone of Duolingo english words exercise' }],
+    },
+  },
+});
